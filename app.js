11e97e9c946e1dec5a9137ab224f6f3c09dd10c7
Deividas <deividasgineitis123@gmail.com>
2020-12-22 14:19:39 +0200

    Add ability to change reading status

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+//get needed DOM elements
 const bookView = document.getElementById("app");
 const addBookForm = document.getElementById("addBookForm");
 const addBookButton = document.getElementById("addBookButton");
 
+//variables
 let isFormShown = false;
-
 let myLibrary = [
     {
         id: 1,
@@ -28,6 +29,7 @@ let myLibrary = [
     }
 ];
 
+//Book object constructor
 function Book(title, author, pages, read) {
   this.id = Date.now();
   this.title = title;
@@ -36,6 +38,7 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+//Add book button event listener
 addBookButton.addEventListener("click", ()=>{
     if(!isFormShown){
         addBookForm.style.display = "flex";
@@ -46,11 +49,9 @@ addBookButton.addEventListener("click", ()=>{
     }  
 });
 
+
 function addBookToLibrary(event) {
     event.preventDefault();
-    //Close add book form
-    addBookForm.style.display = "none";
-    isFormShown = false;
     
     //get values of form
     const title = document.getElementById("bookTitleInput").value;
@@ -61,34 +62,65 @@ function addBookToLibrary(event) {
     //add new book to librart array based on form values
     myLibrary.push(new Book(title, author, pages, read));
 
+    //reset add book form
+    addBookForm.style.display = "none";
+    isFormShown = false;
+    addBookForm.reset();
+    
     //render the library
     render();
     
 }
 
 function createBookEntry({id, title, author, pages, read}){
+    //create entry DOM element
     const entry = document.createElement("div");
     entry.setAttribute("id", id.toString());
     entry.classList.add("library");
 
+    //create Title
     const bookTitle = document.createElement("h2");
     bookTitle.innerHTML = title;
     bookTitle.classList.add("library__title");
     entry.appendChild(bookTitle);
 
+    //create author
     const bookAuthor = document.createElement("p");
     bookAuthor.innerHTML = `Written by ${author}`;
     entry.appendChild(bookAuthor);
 
+    //create pages
     const bookPages = document.createElement("p");
     bookPages.innerHTML = `${pages.toString()} pages`;
     entry.appendChild(bookPages);
 
-    const bookReadingStatus = document.createElement("p");
+    //create reading status
+    const bookReadingStatus = document.createElement("button");
     bookReadingStatus.innerHTML = read ? "Read" : "Not read";
+    bookReadingStatus.classList.add("library__reading-status");
     bookReadingStatus.classList.add(read ? "library__reading-status--read" : "library__reading-status--not-read");
     entry.appendChild(bookReadingStatus);
 
+    bookReadingStatus.addEventListener("click", (event) => {
+        //find the object in our array
+        const foundLibraryElement = myLibrary.find( ({id}) => id == event.target.parentElement.id );
+        //if to change the targeted entry reading status
+        if(foundLibraryElement.read == true){
+            //change reading status in array
+            foundLibraryElement.read = false;
+            //change the class and inner html of selected entry (not object but DOM element)
+            event.target.classList.remove("library__reading-status--read");
+            event.target.classList.add("library__reading-status--not-read");
+            event.target.innerHTML = "Not read";
+        }else if(foundLibraryElement.read == false){
+            foundLibraryElement.read = true;
+            event.target.classList.remove("library__reading-status--not-read");
+            event.target.classList.add("library__reading-status--read");
+            event.target.innerHTML = "Read";
+        }
+    });
+
+    //create delete button
     const deleteButton = document.createElement("Button");
     deleteButton.setAttribute("id", "deleteEntryButton");
     deleteButton.classList.add("library__delete-button", "fa", "fa-trash");
@@ -100,7 +132,7 @@ function createBookEntry({id, title, author, pages, read}){
         myLibrary.splice(index, 1);
         render();
         
-    })
+    });
     
     
     return entry;
